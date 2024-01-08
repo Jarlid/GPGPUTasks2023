6ce0edef10ceaca05243de781216c5c2e0c88dd8
Jarlid <korkiniostudymail@gmail.com>
2024-01-08 06:56:14 +0300

    Half-baked Task 7.

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,3 +1,143 @@
-__kernel void radix(__global unsigned int *as) {
-    // TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+// База:
+
+// Эти штуки зависят друг от друга, мне было лень делать цикл.
+#define OFFSET_SIZE 4
+#define OFFSET_VARIANTS 16
+
+#define WORKGROUP_SIZE 128
+
+unsigned int trimmed_value(const unsigned int value, const unsigned int offset) {
+    return (value >> offset) & (OFFSET_VARIANTS - 1); // Могут получиться значения от 0 до 15.
+}
+
+__kernel void counter(__global const unsigned int *as, __global unsigned int *counter_table, const unsigned int offset) {
+    const unsigned global_id = get_global_id(0);
+    const unsigned local_id = get_local_id(0);
+    const unsigned group_id = get_group_id(0);
+
+    __local unsigned int ct_row[OFFSET_VARIANTS];
+    if (local_id < OFFSET_VARIANTS)
+        ct_row[local_id] = 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    unsigned int trimmed_a = trimmed_value(as[global_id], offset);
+    atomic_add(ct_row + trimmed_a, 1);
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (local_id < OFFSET_VARIANTS)
+        counter_table[group_id * OFFSET_VARIANTS + local_id] = ct_row[local_id];
+}
+
+__kernel void radix_reconstruct(__global unsigned int* as, __global unsigned int* sorted_as,
+                                __global unsigned int* t_global_prefix, __global unsigned int* local_prefix,
+                                const unsigned int n, const unsigned int offset) {
+    const unsigned global_id = get_global_id(0);
+    const unsigned group_id = get_group_id(0);
+
+    if (global_id >= n)
+        return;
+
+    unsigned int trimmed_a = trimmed_value(as[global_id], offset);
+    unsigned int ta_gid = group_id + (n / WORKGROUP_SIZE) * trimmed_a;
+
+    unsigned int final_place = global_id % WORKGROUP_SIZE;
+
+    if (ta_gid > 0) {
+        final_place += t_global_prefix[ta_gid - 1];
+    }
+    if (trimmed_a > 0) {
+        final_place -= local_prefix[group_id * OFFSET_VARIANTS + trimmed_a - 1];
+    }
+
+    if (final_place < n)
+        sorted_as[final_place] = as[global_id];
+}
+
+// Вещи, необходимые для mergesort:
+
+bool comes_before(const unsigned int i, const unsigned int j, __global const unsigned int* as, const unsigned int n, const unsigned int which_half, const unsigned int offset) {
+    if (j >= n)
+        return true;
+    if (which_half == 0)
+        return trimmed_value(as[i], offset) <= trimmed_value(as[j], offset);
+    return trimmed_value(as[i], offset) < trimmed_value(as[j], offset);
+}
+
+__kernel void merge(__global unsigned int* as, __global unsigned int* bs, const unsigned int n, const unsigned int merge_chunk_size, const unsigned int offset)
+{
+    const unsigned int i = get_global_id(0);
+    if (i >= n) return;
+
+    const unsigned int chunk_num = i / (2 * merge_chunk_size);
+    const unsigned int which_half = i / merge_chunk_size % 2; // 0 - первая половина, 1 - вторая.
+    const unsigned int other_half = (which_half + 1) % 2;
+
+    unsigned int l = -1, r = merge_chunk_size;
+    while (r - l > 1) {
+        unsigned int m = (l + r) / 2;
+        if (comes_before(i, (chunk_num * 2 + other_half) * merge_chunk_size + m, as, n, which_half, offset))
+            r = m;
+        else
+            l = m;
+    }
+
+    bs[i + r - which_half * merge_chunk_size] = as[i];
+}
+
+// Транспонирование матрицы:
+
+#define TILE_SIZE 16
+
+__kernel void matrix_transpose(__global const unsigned int* as, __global unsigned int* as_t, unsigned int M, unsigned int K)
+{
+    const unsigned int lid_i = get_local_id(0);
+    const unsigned int lid_j = get_local_id(1);
+    const unsigned int gid_i = get_global_id(0);
+    const unsigned int gid_j = get_global_id(1);
+
+    __local unsigned int tile[TILE_SIZE][TILE_SIZE];
+    tile[lid_j][lid_i] = as[gid_j * K + gid_i];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    const unsigned int wid_i = get_group_id(0);
+    const unsigned int wid_j = get_group_id(1);
+
+    as_t[(wid_i * TILE_SIZE + lid_j) * M + (wid_j * TILE_SIZE) + lid_i] = tile[lid_i][lid_j];
+}
+
+// Высчитывание префиксных сумм:
+
+__kernel void prefix_sum(__global unsigned int *as, const unsigned int n, const unsigned int chunk_size) {
+    const unsigned int i = get_global_id(0);
+
+    const unsigned int index = i + (i / chunk_size + 1) * chunk_size;
+    if (index > n)
+        return;
+
+    const unsigned int addon_index = (2 * (i / chunk_size) + 1) * chunk_size - 1;
+
+    as[index] += as[addon_index];
+}
+
+__kernel void local_prefix_sum(__global unsigned int* as, __global unsigned int* local_prefix_table, const unsigned int n) {
+    const unsigned int id = get_global_id(0);
+    if (id % OFFSET_VARIANTS != 0)
+        return;
+
+    unsigned sum = 0;
+    for (int i = 0; i < OFFSET_VARIANTS; ++i) {
+        if (id + i >= n)
+            break;
+        sum += as[id + i];
+        local_prefix_table[id + i] = sum;
+    }
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -10,7 +10,11 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
+#include <bitset>
 
+// Эти штуки зависят друг от друга, мне было лень делать цикл.
+#define OFFSET_SIZE 4
+#define OFFSET_VARIANTS 16
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
@@ -30,8 +34,8 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    int benchmarkingIters = 1; // TODO: 10
+    unsigned int n = 3 * 1024; // TODO: 32 * 1024 * 1024
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -50,32 +54,114 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
-    gpu::gpu_mem_32u as_gpu;
+
+    unsigned int workGroupSize = 128;
+    unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+    unsigned int M = global_work_size / workGroupSize, K = OFFSET_VARIANTS; // TODO: возможно, необходимо поменять местами.
+    // Но пока что K - ширина изначальной таблицы.
+    std::cout << M << " " << K << std::endl;
+
+    unsigned int transpose_work_group_size = OFFSET_VARIANTS;
+    unsigned int transpose_global_work_size_x = (K + transpose_work_group_size - 1) / transpose_work_group_size * transpose_work_group_size;
+    unsigned int transpose_global_work_size_y = (M + transpose_work_group_size - 1) / transpose_work_group_size * transpose_work_group_size;
+
+    unsigned int prefix_global_work_size = (K * M + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+    gpu::gpu_mem_32u as_gpu, bs_gpu, counter_table_gpu, t_counter_table_gpu, local_prefix_table_gpu;
     as_gpu.resizeN(n);
+    bs_gpu.resizeN(n); // Дополнительный буфер для различных целей.
+
+    counter_table_gpu.resizeN(M * K);
+    t_counter_table_gpu.resizeN(K * M);
+    local_prefix_table_gpu.resizeN(M * K);
 
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
-        radix.compile();
+        ocl::Kernel merge(radix_kernel, radix_kernel_length, "merge");
+        merge.compile();
+        ocl::Kernel counter(radix_kernel, radix_kernel_length, "counter");
+        counter.compile();
+        ocl::Kernel transpose(radix_kernel, radix_kernel_length, "matrix_transpose");
+        transpose.compile();
+        ocl::Kernel global_prefix(radix_kernel, radix_kernel_length, "prefix_sum");
+        global_prefix.compile();
+        ocl::Kernel local_prefix(radix_kernel, radix_kernel_length, "local_prefix_sum");
+        local_prefix.compile();
+        ocl::Kernel radix_reconstruct(radix_kernel, radix_kernel_length, "radix_reconstruct");
+        radix_reconstruct.compile();
+
+        // TODO: delete.
+        std::vector<unsigned int> counter_table(M * K, 0);
+        std::vector<unsigned int> t_counter_table(M * K, 0);
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
 
-            t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
+            t.restart(); // Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            // TODO
+            for (int offset = 0; offset < 1; offset += OFFSET_SIZE) { // TODO: max offset.
+
+                // Сортировка значений в небольших блоках с помощью mergesort.
+                for (unsigned int merge_chunk_size = 1; merge_chunk_size < workGroupSize; merge_chunk_size *= 2) {
+                    merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bs_gpu, n, merge_chunk_size, offset);
+                    std::swap(as_gpu, bs_gpu);
+                }
+
+                counter.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, counter_table_gpu, offset);
+
+                std::cout << transpose_work_group_size << " " << transpose_global_work_size_x << " " << transpose_global_work_size_y << std::endl;
+
+                transpose.exec(gpu::WorkSize(transpose_work_group_size, transpose_work_group_size,
+                                             transpose_global_work_size_x, transpose_global_work_size_y),
+                               counter_table_gpu, t_counter_table_gpu, M, K);
+
+                // TODO: delete.
+                counter_table_gpu.readN(counter_table.data(), M * K);
+                t_counter_table_gpu.readN(t_counter_table.data(), M * K);
+
+                // Делает из счётчика индикатор количества до.
+                for (unsigned int chunk_size = 1; chunk_size < K * M; chunk_size *= 2)
+                    global_prefix.exec(gpu::WorkSize(workGroupSize, prefix_global_work_size), t_counter_table_gpu, K * M, chunk_size);
+
+                local_prefix.exec(gpu::WorkSize(workGroupSize, prefix_global_work_size), counter_table_gpu, local_prefix_table_gpu, K * M);
+
+                radix_reconstruct.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                                       as_gpu, bs_gpu, t_counter_table_gpu, local_prefix_table_gpu,
+                                       n, offset);
+
+                std::swap(as_gpu, bs_gpu);
+            }
+
+            t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
 
         as_gpu.readN(as.data(), n);
+
+        // TODO: delete.
+        for (int m = 0; m < M; ++m) {
+            for (int k = 0; k < K; ++k) {
+                std::cout << counter_table[m * K + k] << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << std::endl;
+
+        for (int k = 0; k < K; ++k) {
+            for (int m = 0; m < M; ++m) {
+                std::cout << t_counter_table[k * M + m] << " ";
+            }
+            std::cout << std::endl;
+        }
     }
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
